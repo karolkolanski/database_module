da05e346449debba28dbd23a8f1a75a2a165e37d
Karol Kolanski <gralu86@gmail.com>
2022-01-22 15:14:25 +0100

    Connection established

---
+++ database_module.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+class DatabaseConnector:
+    def __init__(self, address="localhost", username="root", password="automaty", database_name="alk"):
+        # Połączenie z bazą danych
+        try:
+            self.mydb = mysql.connector.connect(
+                host = address,
+                user = username,
+                password = password,
+                database = database_name
+            )
+            self.cursor = self.mydb.cursor()
+            print("Connection extablished successfully")
+        except:
+            raise RuntimeError("Cannot connect to the database")
+
+db = DatabaseConnector()
+
